8631c35eb0e075b461eb10e90817907f51c359f2
Éric Tanter <etanter@gmail.com>
2020-09-04 13:29:46 -0400

    open alcotest anyway, so once at toplevel

--- bin/test.ml
+++ bin/test.ml
@@ -1,43 +1,42 @@
 open Compiler.Interp
+open Alcotest
 
 (* Testing arithmetic expression using the print function defined in Interp 
    and the default equality for comparison *)
-let exp : exp Alcotest.testable =
-  Alcotest.testable pp_exp (=)
+let exp : exp testable =
+  testable pp_exp (=)
 
-let value : value Alcotest.testable =
-  Alcotest.testable pp_value (=)
+let value : value testable =
+  testable pp_value (=)
 
 
 (* Tests for our [parse] function *)
 let test_parse_int () =
-  Alcotest.check exp "same int" (parse (`Atom "5")) (Num 5)
+  check exp "same int" (parse (`Atom "5")) (Num 5)
 
 let test_parse_var () =
-  Alcotest.check exp "same var" (parse (`Atom "x")) (Var "x")
+  check exp "same var" (parse (`Atom "x")) (Var "x")
 
 let test_parse_compound () =
-  Alcotest.check exp "same expr"
+  check exp "same expr"
     (parse (`List [`Atom "+" ; `List [`Atom "*" ; `Atom "3"; `Atom "x"]; `Atom "7"]))
     (Plus (Times (Num 3, Var "x"), Num 7))
 
 let test_parse_error () =
   let sexp = `List [`Atom "foo"; `Atom "bar"] in
-  Alcotest.check_raises "Should raise failwith" 
+  check_raises "Should raise failwith" 
     (Failure (Fmt.strf "Not a valid exp: %a" CCSexp.pp sexp))
     (fun () -> ignore @@ parse sexp)
 
-
-
 (* Tests for our [interp] function *)
 let test_interp_num () =
-  Alcotest.(check value) "same int" (interp empty_env (Num 42)) (NumV 42)
+  (check value) "same int" (interp empty_env (Num 42)) (NumV 42)
 
 let test_interp_var () =
-  Alcotest.(check value) "same int" (interp ["x", NumV 7] (Var "x")) (NumV 7)
+  (check value) "same int" (interp ["x", NumV 7] (Var "x")) (NumV 7)
 
 let test_interp_compound () =
-  Alcotest.(check value) "same int"
+  (check value) "same int"
     (interp empty_env (Plus (Times (Num 3, Num 5), Num 12)))
     (NumV 27)
 
@@ -46,7 +45,6 @@ let test_interp_compound () =
  * arguments in [run].
  * See the documentation at https://github.com/mirage/alcotest *)
 let () =
-  let open Alcotest in
   run "Interp"
     [
       "parse", [
